e6da81ee0ef9cb00856db2bf3697c1cf23a62a95
Aditya <aditya.shukla278@gmail.com>
2017-12-21 18:58:01 +0530

    Added Sprites along with basic Scripts

---
+++ Balance The bottle/Assets/Scripts/BottleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BottleScript : MonoBehaviour {
+
+    public Transform spawnpoint;
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = 1;
+        }
+
+        if(transform.position.y <= -5f)
+        {
+            //Restart
+            SceneManager.LoadScene("Scene1");
+        }
+
+	}
+}
---
+++ Balance The bottle/Assets/Scripts/FingerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerScript : MonoBehaviour {
+
+    bool wasJustClicked = true;
+    bool canMove;
+    Vector2 playerSize;
+    public Rigidbody2D rb;
+
+    public Transform BoundaryPoints;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerSize = gameObject.GetComponent<SpriteRenderer>().bounds.extents;
+        rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (wasJustClicked)
+            {
+                wasJustClicked = false;
+
+                if ((mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x ||
+                mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) &&
+                (mousePos.y >= transform.position.y && mousePos.y < transform.position.y + playerSize.y ||
+                mousePos.y <= transform.position.y && mousePos.y > transform.position.y - playerSize.y))
+                {
+                    canMove = true;
+                }
+                else
+                {
+                    canMove = false;
+                }
+            }
+
+            if (canMove)
+            {
+                transform.position = mousePos;
+
+                //rb.MovePosition(mousePos);
+            }
+        }
+        else
+        {
+            wasJustClicked = true;
+        }
+    }
+}
